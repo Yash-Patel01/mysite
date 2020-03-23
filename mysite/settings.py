65254e749b1dff32b8d74b8cc8833c7a851c03d9
Yash-Patel01 <55884549+Yash-Patel01@users.noreply.github.com>
2020-03-24 00:05:48 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = []
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'nikpatel7048@gmail.com'
-EMAIL_HOST_PASSWORD = 'Yash@63550'
+EMAIL_HOST_USER = 'your_gmail_address@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
 EMAIL_PORT = 587
 
 # Application definition
